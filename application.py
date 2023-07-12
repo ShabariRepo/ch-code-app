c57ef84ce54389841bb39975a347ce87c8dc4817
ShabariRepo <cupicake@catherines-mbp.lan>
2023-07-11 21:05:43 -0400

    - add sleep for 30

--- application.py
+++ application.py
@@ -1,7 +1,13 @@
 # use flask as simple backend to get things running faster
 # boilerplate python hello world structrue
 
+# import time for sleep or wait function
+import time
 from flask import Flask
+
+# lazy but didnt really find a better way that is not 100 lines of code & this was simple :S
+time.sleep(30)
+
 app = Flask(__name__)
 
 @app.route('/')
